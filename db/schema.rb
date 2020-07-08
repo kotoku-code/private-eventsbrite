a5c15feb6c0291c3aa832b588cf86e6d92cd12ac
kotoku-code <kennedy.kotoku@gmail.com>
2020-07-08 11:38:33 +0200

    views user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_084722) do
+ActiveRecord::Schema.define(version: 2020_07_08_091650) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'digest' for column 'password'
 
 end
