d65c8f89c17fb251caa4288fcba5a5148293757f
kotoku-code <kennedy.kotoku@gmail.com>
2020-07-08 12:41:24 +0200

    removed params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     end
 
     def create
-      user = User.new(user_params)
+      user = User.new
       if user.save
         session[:user_id] = user.id
         redirect_to '/dashboard'
